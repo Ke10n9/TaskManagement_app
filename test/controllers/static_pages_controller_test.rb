f5263f0da20145ff04090a789e540400587c3e45
Nagai Kensuke <kn.10n9@gmail.com>
2020-04-04 16:16:56 +0900

    Finish static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,14 +1,22 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
-  test "should get home" do
-    get static_pages_home_url
+  test "should get root" do
+    get root_url
     assert_response :success
+    assert_select "title", "Home|Task Management App"
   end
 
-  test "should get help" do
-    get static_pages_help_url
-    assert_response :success
-  end
+  # test "should get help" do
+  #   get static_pages_help_url
+  #   assert_response :success
+  #   assert_select "title", "Help|Task Management App"
+  # end
+  #
+  # test "should get about" do
+  #   get static_pages_about_url
+  #   assert_response :success
+  #   assert_select "title", "About|Task Management App"
+  # end
 
 end
