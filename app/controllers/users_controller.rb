9167992269d9862a29532f90b0ae658405c67184
Nagai Kensuke <kn.10n9@gmail.com>
2020-04-10 20:26:03 +0900

    Use SSL and the Puma webserver in production

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-    if false #@user.save
+    if @user.save
       flash[:success] = "Welcome to the Menu Diary!"
       redirect_to @user
     else
