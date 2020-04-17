dfc5ba56c4172da3d7cc4e994c551b9a2164853b
Nagai Kensuke <kn.10n9@gmail.com>
2020-04-17 22:21:35 +0900

    Add account activation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,11 +4,12 @@ class UsersController < ApplicationController
   before_action :admin_user, only: :destroy
 
   def index  #管理者のみ参照可能にしたい
-    @users = User.paginate(page: params[:page])
+    @users = User.where(activated: true).paginate(page: params[:page])
   end
 
   def show
     @user = User.find(params[:id])
+    redirect_to root_url and return unless @user.activated?
   end
 
   def new
@@ -18,9 +19,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      log_in @user
-      flash[:success] = "Welcome to the Menu Diary!"
-      redirect_to @user
+      @user.send_activation_email  #app/models/user.rb
+      flash[:info] = "Please check your email to activate your account."
+      redirect_to root_url
     else
       render 'new'
     end
